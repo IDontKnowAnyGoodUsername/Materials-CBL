1289fcafbcc8f08dfda723f49d107ee98d8acc31
IDontKnowAnyGoodUsername <168458418+IDontKnowAnyGoodUsername@users.noreply.github.com>
2025-05-27 13:40:58 +0200

    begin callback

--- src/example_input.c
+++ src/example_input.c
@@ -44,11 +44,15 @@ int main(void) {
 		return 1;
 	}
 
-	printf("Channel 0: %s\n", value[0] ? "true" : "false");
+	for(int i = 0; i <100; i++){
+		io4_v2_get_value(&io, value);
+		printf("%d, %d\n", value[0],value[1]);
+	}
+	/*printf("Channel 0: %s\n", value[0] ? "true" : "false");
 	printf("Channel 1: %s\n", value[1] ? "true" : "false");
 	printf("Channel 2: %s\n", value[2] ? "true" : "false");
 	printf("Channel 3: %s\n", value[3] ? "true" : "false");
-
+	*/
 	/*
 	bool button[BITSIZE];
 	char cmd;
@@ -66,12 +70,56 @@ int main(void) {
 		scanf(" %c", &cmd);
 	}
 		*/
+	/*int pulse_counter = 0;
+		for (int i = 0; i < 300; i++) {
+    	int counter = 0;
 
-		for(int i = 0; i <1000; i++){
+    	// Wait for clock to go LOW
+    	do {
+        	io4_v2_get_value(&io, value);
+        	Sleep(1);  // 0.1 ms = avoid CPU hammering
+    	} while (value[1] == 1);
+    	// Clock is LOW, measure how long it's LOW
+    	do {
+        	io4_v2_get_value(&io, value);
+        	Sleep(1);  // 0.1 ms
+        	counter++;
+    	} while (value[1] == 0);
+
+   	 // Clock went HIGH = rising edge = sample data
+    	if (counter > 5) {
+    	    printf("   %d pulses,(clock low for ~%d * 100us)\n", pulse_counter, counter);
+			pulse_counter =0;
+    	}
+
+    	// Sample data at the moment clock goes high
+    	printf("%d", value[0]);
+	}*/
+	bool string[300];
+	for(int i = 0; i<300; i++){
+		io4_v2_get_value(&io, value);
+		string[i] = value[1];
+	}
+	for(int i = 0; i <300; i++){
+		printf("%d", string[i]);
+	}
+
+	/*for(int i = 0; i <50; i++){
+		int zero_time =0;
+		//bool string[300];
+		do{
 			io4_v2_get_value(&io, value);
-			printf("%d", value[0]);
-
-		}
+			while(value[1]==0){		//wait until high
+				io4_v2_get_value(&io, value);
+				zero_time++;
+			}
+			printf("%d", value[0]);				//sample at edge
+			while(value[1] ==1){	//wait until low
+				io4_v2_get_value(&io, value);
+			}
+		} while (zero_time<8);
+		printf("\n");
+	}*/
 
 	io4_v2_destroy(&io);
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
